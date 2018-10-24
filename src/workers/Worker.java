52dcc69747e01dab2e12ce21832fb1f769410d3b
Ailin Patiño <ailinsp50@gmail.com>
2018-10-24 20:14:16 -0300

    agrego package worker

---
+++ src/workers/Worker.java
@@ -0,0 +1,36 @@
+package workers;
+
+public abstract class Worker extends Thread {
+
+	private Buffer buffer;
+	private ThreadPool tpool;
+	
+	public Worker(Buffer buffer, ThreadPool tpool) {
+
+		this.buffer = buffer;
+		this.tpool = tpool;
+	}
+
+	public Buffer getBuffer() {
+		return buffer;
+	}
+
+	@Override
+	public void run() {
+		while (true){
+			this.work();
+		}
+	}
+
+	public abstract void work();
+
+	public void setBegin(int n) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setEnd(int capacidad) {
+		// TODO Auto-generated method stub
+		
+	}
+}
---
+++ src/workers/WorkerAdd.java
@@ -0,0 +1,21 @@
+package workers;
+
+public class WorkerAdd extends Worker {
+
+    private ConcurVector v2;
+
+    public WorkerAdd(ConcurVector v, ConcurVector v2) {
+        super(v,cant, barrier);
+        this.v2 = v2;
+    }
+
+    @Override
+    public void work(){
+        for(int cant = 0; cant < this.cantToAnalize; cant++){
+            int positionToAnalize = this.assignedVector.getPositionToAnalize();
+            this.assignedVector.set(positionToAnalize, this.assignedVector.get(positionToAnalize) + v2.get(positionToAnalize));
+            this.barrier.subCant();
+        }
+        return;
+    }
+}
